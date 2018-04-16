46b38e638ac0eb21b11bdb05b6fe67bac8b25910
SHERlocked93 <443033099@qq.com>
2018-04-16 21:51:21 +0800

    多项条件选择组件更新

---
+++ src/utils/index.js
@@ -0,0 +1,158 @@
+/**
+ * 创建于 2018/4/16
+ * 作者: SHERlocked93
+ * 功能: 工具库函数
+ */
+
+/**
+ * Base64工具库
+ * @example 编码 - Base64.encode("hello wrold")
+ * @example 解码 - Base64.decode("d3d3Ljc4b2EuY29t")
+ * @type {Object}
+ */
+export const Base64 = {
+  _keyStr: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
+  
+  // public method for encoding
+  encode: function(input) {
+    let output = ''
+    let chr1, chr2, chr3, enc1, enc2, enc3, enc4
+    let i = 0
+    
+    input = Base64._utf8_encode(input)
+    
+    while (i < input.length) {
+      chr1 = input.charCodeAt(i++)
+      chr2 = input.charCodeAt(i++)
+      chr3 = input.charCodeAt(i++)
+      
+      enc1 = chr1 >> 2
+      enc2 = ((chr1 & 3) << 4) | (chr2 >> 4)
+      enc3 = ((chr2 & 15) << 2) | (chr3 >> 6)
+      enc4 = chr3 & 63
+      
+      if (isNaN(chr2)) {
+        enc3 = enc4 = 64
+      } else if (isNaN(chr3)) {
+        enc4 = 64
+      }
+      
+      output = output +
+        this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+        this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4)
+    }
+    
+    return output
+  },
+  
+  // public method for decoding
+  decode: function(input) {
+    let output = ''
+    let chr1, chr2, chr3
+    let enc1, enc2, enc3, enc4
+    let i = 0
+    
+    input = input.replace(/[^A-Za-z0-9+\/=]/g, '')
+    
+    while (i < input.length) {
+      enc1 = this._keyStr.indexOf(input.charAt(i++))
+      enc2 = this._keyStr.indexOf(input.charAt(i++))
+      enc3 = this._keyStr.indexOf(input.charAt(i++))
+      enc4 = this._keyStr.indexOf(input.charAt(i++))
+      
+      chr1 = (enc1 << 2) | (enc2 >> 4)
+      chr2 = ((enc2 & 15) << 4) | (enc3 >> 2)
+      chr3 = ((enc3 & 3) << 6) | enc4
+      
+      output = output + String.fromCharCode(chr1)
+      
+      if (enc3 !== 64) {
+        output = output + String.fromCharCode(chr2)
+      }
+      if (enc4 !== 64) {
+        output = output + String.fromCharCode(chr3)
+      }
+    }
+    
+    output = Base64._utf8_decode(output)
+    
+    return output
+  },
+  
+  // private method for UTF-8 encoding
+  _utf8_encode: function(string) {
+    string = string.replace(/\r\n/g, '\n')
+    let utftext = ''
+    
+    for (let n = 0; n < string.length; n++) {
+      const c = string.charCodeAt(n)
+      
+      if (c < 128) {
+        utftext += String.fromCharCode(c)
+      } else if ((c > 127) && (c < 2048)) {
+        utftext += String.fromCharCode((c >> 6) | 192)
+        utftext += String.fromCharCode((c & 63) | 128)
+      } else {
+        utftext += String.fromCharCode((c >> 12) | 224)
+        utftext += String.fromCharCode(((c >> 6) & 63) | 128)
+        utftext += String.fromCharCode((c & 63) | 128)
+      }
+    }
+    
+    return utftext
+  },
+  
+  // private method for UTF-8 decoding
+  _utf8_decode: function(utftext) {
+    let string = ''
+    let i = 0
+    let c = 0
+    let c2 = 0
+    let c3 = 0
+    
+    while (i < utftext.length) {
+      c = utftext.charCodeAt(i)
+      
+      if (c < 128) {
+        string += String.fromCharCode(c)
+        i++
+      } else if ((c > 191) && (c < 224)) {
+        c2 = utftext.charCodeAt(i + 1)
+        string += String.fromCharCode(((c & 31) << 6) | (c2 & 63))
+        i += 2
+      } else {
+        c2 = utftext.charCodeAt(i + 1)
+        c3 = utftext.charCodeAt(i + 2)
+        string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63))
+        i += 3
+      }
+    }
+    
+    return string
+  }
+  
+}
+
+/* 输入时间戳 按照fmt格式化
+* fmt为格式 ex: yyyy-MM-dd hh:mm:ss => 2018-07-23 21:52:44
+*/
+export const formatTime = (timestamp, fmt) => {
+  const time = new Date(timestamp)
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (time.getFullYear() + '').substr(4 - RegExp.$1.length))
+  }
+  const types = {
+    'M+': time.getMonth() + 1,
+    'd+': time.getDate(),
+    'h+': time.getHours(),
+    'm+': time.getMinutes(),
+    's+': time.getSeconds()
+  }
+  for (const type in types) {
+    if (new RegExp(`(${type})`).test(fmt)) {
+      const str = types[type] + ''
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : `00${str}`.substr(str.length))
+    }
+  }
+  return fmt
+}
